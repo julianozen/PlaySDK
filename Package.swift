d9bd585134db1cb7b8f6c1024bdd3087dacdc540
Tom OMalley <9475035+tomalley104@users.noreply.github.com>
2025-03-10 19:05:47 -0400

    [0.1.0-beta] initial source commit w/ linked binary in release

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PlaySDK",
+    platforms: [.iOS(.v16), .macCatalyst(.v16),],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PlaySDK",
+            targets: ["PlaySDKWrapper"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PlaySDKWrapper",
+            dependencies: ["PlaySDKBinary"]
+        ),
+        .binaryTarget(
+          name: "PlaySDKBinary",
+          url: "https://github.com/CreateWithPlayApp/PlaySDK/releases/download/v0.1.0-beta/_PlaySDK.xcframework.zip",
+          checksum: "c988c7dc4f558ee3044e609b6d6e63c9f9ac3775b0981e6e223977672e727392"
+        )
+    ]
+)
